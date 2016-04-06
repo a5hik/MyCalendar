98e442dd7a44b1276b04eff0f93ec75768f25236
ashik <ashikes@gmail.com>
2016-04-06 17:24:22 +0530

    added the required controllers.

--- MyCalendar/MonthsTableViewController.swift
+++ MyCalendar/MonthsTableViewController.swift
@@ -11,4 +11,18 @@ import UIKit
 
 class MonthsTableViewController : UITableViewController {
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Basic")!
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
 }
